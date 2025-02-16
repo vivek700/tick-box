35dbb460f7f7f0a6923c3525bfed6b9c80ac49d2
Vivek <vs690799@gmail.com>
2025-02-16 21:46:26 +0530

    merge conflit fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,9 @@
 import type { PageServerLoad } from './$types'
 
-<<<<<<< HEAD
-export const load: PageServerLoad = async ({ locals }) => {
-	const access_code = locals.access_code
-=======
 export const load: PageServerLoad = async ({ cookies }) => {
-	const access_code = await cookies.get("access_code")
+	const access_code = cookies.get("access_code")
 	console.log(access_code)
 
->>>>>>> dev
 	return {
 		access_code
 	}
