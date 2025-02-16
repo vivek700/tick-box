39c895b5fc34fa4949403a11d192a7e06d73e4aa
Vivek <vs690799@gmail.com>
2025-02-16 15:57:38 +0530

    set secure option

--- src/routes/connect/[slug]/+page.server.ts
+++ src/routes/connect/[slug]/+page.server.ts
@@ -7,7 +7,7 @@ export const load: PageServerLoad = async ({ params, cookies }) => {
 	if (typeof access_code_value === 'string' && access_code_value.length > 0) {
 		cookies.set("access_code", access_code_value, {
 			path: "/",
-			secure: false,
+			secure: true,
 			httpOnly: true,
 			expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000)
 		})
