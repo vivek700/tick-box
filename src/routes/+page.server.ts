40cbdc1345583ef2c4f6aa6001d925ab5cf6a149
Vivek <vs690799@gmail.com>
2025-02-16 20:34:34 +0530

    updated code

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ cookies }) => {
 	const access_code = cookies.get("access_code")
-
+	console.log(access_code)
 	return {
 		access_code
 	}
