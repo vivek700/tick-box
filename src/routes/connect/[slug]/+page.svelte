aed40b0ecaff82c7a31ada084e6211228da7c959
Vivek <vs690799@gmail.com>
2025-02-17 16:50:46 +0530

    updated the files and code

--- src/routes/connect/[slug]/+page.svelte
+++ src/routes/connect/[slug]/+page.svelte
@@ -1,1 +1,23 @@
-<h1>Wait...</h1>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { PageProps } from './$types';
+	import { PUBLIC_BASE_URL } from '$env/static/public';
+	import { error } from '@sveltejs/kit';
+	import { goto } from '$app/navigation';
+
+	let { data }: PageProps = $props();
+
+	onMount(async () => {
+		const res = await fetch(`${PUBLIC_BASE_URL}/set-cookie/${data.access_code_value}`, {
+			method: 'POST',
+			credentials: 'include'
+		});
+		if (!res.ok) {
+			error(res.status, {
+				message: 'Unable to save your preferences. Please clear your browser cookies and try again.'
+			});
+		} else {
+			goto('/');
+		}
+	});
+</script>
